5459cd0e6947ce07cc2b64107691ddd7f0a6d5fc
Ken Taylor <kentaylosappdev@gmail.com>
2024-01-22 17:05:55 -0400

    test findMany

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,14 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma 
+import prisma from "@/app/prisma/Prisma";
 export async function GET(request: NextRequest) {
   try {
-    const data = await prisma.file.findUnique({
+    const data = await prisma.file.findMany({
       where: {
         userId: "user_2XM8vsWSmFUAg1gxZGeqtEB99Yw",
       },
     });
     console.log(data);
-    return NextResponse.json({ success: true });
+    return NextResponse.json({ data });
   } catch (error) {
     console.log(error);
   }
