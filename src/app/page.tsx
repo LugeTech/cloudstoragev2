cab917899dcb8fc0bcdd540b77c8622fd62d61e7
Clint <clint4life@gmail.com>
2024-01-22 15:24:50 -0500

    removed sign in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,9 @@ import { UserButton, useUser } from "@clerk/nextjs";
 import { FrontPage } from "@/components/front-page";
 
 export default function Home() {
-  const { isLoaded, isSignedIn } = useUser();
-  if (!isLoaded || !isSignedIn) {
-    return (
-      <button onClick={() => (window.location.href = "/sign-in")}>
-        Sign In
-      </button>
-    );
-  } else {
-    return (
-      <main className="flex min-h-screen w-full flex-col items-center sm:px-24 ">
-        <FrontPage />
-      </main>
-    );
-  }
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center sm:px-24 ">
+      <FrontPage />
+    </main>
+  );
 }
